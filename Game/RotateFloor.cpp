48e934a9326501b5863c67b5db8cc66966679372
MSI\GSManager <unonnk@g.neec.ac.jp>
2024-06-17 18:00:42 +0900

    離島の壁を追加

--- Game/RotateFloor.cpp
+++ Game/RotateFloor.cpp
@@ -5,43 +5,43 @@
 #include "VertexData.h"
 #include "Math/MyMath.h"
 
-RotateFloor::~RotateFloor(){
+RotateFloor::~RotateFloor() {
 	delete mTexture;
 	delete mModel;
 }
 
-void RotateFloor::Initialize(DirectXCommon* dxCommon){
+void RotateFloor::Initialize(DirectXCommon* dxCommon) {
 	mDxCommon = dxCommon;
 	mTransform = {
 		{1.0f,1.0f,1.0f},
-		{0.0f,kPi/2.0f,0.0f},
+		{0.0f,kPi / 2.0f,0.0f},
 		{15.0f,1.5f,57.5f}
 	};
 	mModel = new Model();
 	mModel->Create(mDxCommon, "resources/Model/Bridge", "Bridge.obj");
 }
 
-void RotateFloor::Update(){
+void RotateFloor::Update() {
 	Vector3 sideLWorldPos = { 6.5f,0.0f,0.0f };
 	Vector3 sideRWorldPos = { -6.5f,0.0f,0.0f };
 	Matrix4x4 rotateX = MakeRotateXMatrix(mTransform.rotate.x);
 	Matrix4x4 rotateY = MakeRotateYMatrix(mTransform.rotate.y);
 	Matrix4x4 rotateZ = MakeRotateZMatrix(mTransform.rotate.z);
 	Matrix4x4 rotateMatrix = Multiply(Multiply(rotateX, rotateY), rotateZ);
-	sideLWorldPos = Multiply(sideLWorldPos,rotateMatrix);
+	sideLWorldPos = Multiply(sideLWorldPos, rotateMatrix);
 	sideRWorldPos = Multiply(sideRWorldPos, rotateMatrix);
 	Vector3 WorldPos = GetWorldPosition();
 	mSideLOBB = CalcurateOBB(sideLWorldPos + WorldPos, { 1.0f,10.0f,30.0f });
 	mSideROBB = CalcurateOBB(sideRWorldPos + WorldPos, { 1.0f,10.0f,30.0f });
-	mOBB = CalcurateOBB(WorldPos,{5.5f,0.1f,30.0f});
+	mOBB = CalcurateOBB(WorldPos, { 5.5f,0.1f,30.0f });
 	mTransform.UpdateMatrix();
 }
 
-void RotateFloor::Draw(ID3D12GraphicsCommandList* commandList, Camera* camera){
+void RotateFloor::Draw(ID3D12GraphicsCommandList* commandList, Camera* camera) {
 	mModel->Draw(commandList, camera, mTransform);
 }
 
-OBB RotateFloor::CalcurateOBB(const Vector3& translate,const Vector3& size){
+OBB RotateFloor::CalcurateOBB(const Vector3& translate, const Vector3& size) {
 	OBB ret;
 	ret.center = translate;
 	ret.size = size;
@@ -55,7 +55,7 @@ OBB RotateFloor::CalcurateOBB(const Vector3& translate,const Vector3& size){
 	ret.axis[1].x = rotateMatrix.m[1][0];
 	ret.axis[1].y = rotateMatrix.m[1][1];
 	ret.axis[1].z = rotateMatrix.m[1][2];
-ret.axis[2].x = rotateMatrix.m[2][0];
+	ret.axis[2].x = rotateMatrix.m[2][0];
 	ret.axis[2].y = rotateMatrix.m[2][1];
 	ret.axis[2].z = rotateMatrix.m[2][2];
 	return ret;
